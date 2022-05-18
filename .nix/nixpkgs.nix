b37e0b010e71115461e43db1a3e0cea419c548e5
Pierre Roux <pierre@roux01.fr>
2022-05-18 12:04:19 +0200

    [CI] Try Coq master in Nix CI

---
+++ .nix/nixpkgs.nix
@@ -0,0 +1,4 @@
+fetchTarball {
+         url = https://github.com/CohenCyril/nixpkgs/archive/72a036058777a31ece35c2996964794109795c9c.tar.gz;
+         sha256 = "1xzyh24j4krm4kmpliqb8lsi1ihfdjiwci0s0904xr04shr85nx1";
+       }
